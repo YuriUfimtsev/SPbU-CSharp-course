0ab96557176bd41245053687ad665b0fa8cd017b
YuriUfimtsev <ufimtsev3007@gmail.com>
2022-09-19 00:27:50 +0300

    implements ParallelMatrixMultiplication

---
+++ Homework1/ParallelMatrixMultiplication/Program.cs
@@ -0,0 +1,84 @@
+﻿using System.Diagnostics;
+using System.Text;
+using ParallelMatrixMultiplication;
+
+const int N = 10;
+
+void SaveResultsInTable(
+    string fileName,
+    (double MathematicalExpectation,
+    double StandardDeviation)[] coherentMatrixMultiplicationEvaluation,
+    (double MathematicalExpectation,
+    double StandardDeviation)[] parallelMatrixMultiplicationEvaluation)
+{
+    using (StreamWriter fileStream = new(fileName))
+    {
+        var lineForFile = "Matrices sizes: 500*1300x1300*500  1000*1300x1300*1000 1500*1300x1300*1500";
+        fileStream.WriteLine(lineForFile);
+        var mutableLineForFile = WriteMathematicalExpectationAndStandardDeviationInFile(coherentMatrixMultiplicationEvaluation, true);
+        fileStream.WriteLine(mutableLineForFile);
+        mutableLineForFile = WriteMathematicalExpectationAndStandardDeviationInFile(parallelMatrixMultiplicationEvaluation, false);
+        fileStream.WriteLine(mutableLineForFile);
+    }
+}
+
+StringBuilder WriteMathematicalExpectationAndStandardDeviationInFile(
+    (double MathematicalExpectation,
+    double StandardDeviation)[] matrixMultiplicationEvaluation,
+    bool isCoherentMultiply)
+{
+    var mutableLineForFile = new StringBuilder();
+    mutableLineForFile.AppendLine(isCoherentMultiply ? "Coherent multiply: " : "Parallel multiply: ");
+    for (var i = 0; i < 3; ++i)
+    {
+        mutableLineForFile = mutableLineForFile.AppendLine($"u={matrixMultiplicationEvaluation[i].MathematicalExpectation}"
+            + $";σ={matrixMultiplicationEvaluation[i].StandardDeviation} ");
+    }
+
+    return mutableLineForFile;
+}
+
+(double MathematicalExpectation, double StandardDeviation)[] FillMatrixMultiplicationEvaluationArray(bool isCoherentMultiplication)
+{
+    var matrixMultiplicationEvaluation
+        = new (double MathematicalExpectation, double StandardDeviation)[3];
+    for (var i = 0; i < 3; ++i)
+    {
+        var stopwatch = new Stopwatch();
+        var randomVariableValues = new double[N];
+        for (var j = 0; j < N; ++j)
+        {
+            var firstMultiplier = new Matrix($"..\\..\\..\\Data{i + 1}_firstMultiplier.txt");
+            var secondMultiplier = new Matrix($"..\\..\\..\\Data{i + 1}_secondMultiplier.txt");
+            stopwatch.Start();
+            var multiplicationResult = isCoherentMultiplication
+                ? firstMultiplier.CoherentMultiplyMatrices(secondMultiplier)
+                : firstMultiplier.ParallelMultiplyMatrices(secondMultiplier);
+            stopwatch.Stop();
+            double time = Convert.ToDouble(stopwatch.Elapsed.Milliseconds);
+            randomVariableValues[j] = time;
+        }
+
+        var evaluation = new Evaluation(randomVariableValues, Convert.ToDouble(1 / N));
+        matrixMultiplicationEvaluation[i]
+            = (evaluation.EvaluateMathematicalExpectation(), evaluation.EvaluateStandardDeviation());
+    }
+
+    return matrixMultiplicationEvaluation;
+}
+
+var matricesArray = Array.CreateInstance(typeof(Matrix), 3);
+var matrixMultipliersArray = Array.CreateInstance(typeof(Matrix), 3);
+for (var i = 0; i < 3; ++i)
+{
+    var firstMultiplier = Matrix.CreateRandomMatrix((i + 1) * 500, 1300);
+    var secondMultiplier = Matrix.CreateRandomMatrix(1300, (i + 1) * 500);
+    matricesArray.SetValue(firstMultiplier, i);
+    matrixMultipliersArray.SetValue(secondMultiplier, i);
+    firstMultiplier.SaveToFile($"..\\..\\..\\Data{i + 1}_firstMultiplier.txt");
+    secondMultiplier.SaveToFile($"..\\..\\..\\Data{i + 1}_secondMultiplier.txt");
+}
+
+var coherentMatrixMultiplicationEvaluation = FillMatrixMultiplicationEvaluationArray(true);
+var parallelMatrixMultiplicationEvaluation = FillMatrixMultiplicationEvaluationArray(false);
+SaveResultsInTable("..\\..\\..\\Report.txt", coherentMatrixMultiplicationEvaluation, parallelMatrixMultiplicationEvaluation);
---
+++ Homework1/ParallelMatrixMultiplicationTests/EvaluationTests.cs
@@ -0,0 +1,27 @@
+﻿using ParallelMatrixMultiplication;
+
+namespace ParallelMatrixMultiplicationTests;
+
+public class EvaluationTests
+{
+    [Test]
+    public void NullProbabilityTest()
+    {
+        double[] randomVariableValuesArray = { 1, 3, 6 };
+        Assert.Throws<ArgumentException>(() => new Evaluation(randomVariableValuesArray, 0));
+    }
+
+    [Test]
+    public void StandartEvaluationTest()
+    {
+        double[] randomVariableValuesArray = { 1, 4, 9, 15 };
+        var evaluation = new Evaluation(randomVariableValuesArray, 0.25);
+        var mathematicalExpectation = evaluation.EvaluateMathematicalExpectation();
+        Assert.That(mathematicalExpectation, Is.EqualTo(7.25));
+        var variance = evaluation.EvaluateVariance();
+        Assert.That(Math.Round(variance, 2), Is.EqualTo(28.19));
+        var standardDeviation = evaluation.EvaluateStandardDeviation();
+        Assert.That(standardDeviation, Is.EqualTo(5.31));
+    }
+}
+

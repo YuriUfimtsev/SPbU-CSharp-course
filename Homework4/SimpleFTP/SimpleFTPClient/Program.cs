4177c556597c263e854ad266554d456a11c582a7
YuriUfimtsev <ufimtsev3007@gmail.com>
2022-11-09 11:20:19 +0300

    initial commit

---
+++ Homework4/SimpleFTP/SimpleFTPClient/Program.cs
@@ -0,0 +1,12 @@
+﻿using System.Net.Sockets;
+
+const int port = 8888;
+using (var client = new TcpClient("localhost", port))
+{
+    Console.WriteLine($"Sending to port {port}...");
+    var stream = client.GetStream();
+    var writer = new StreamWriter(stream);
+    writer.Write("Hello, world!111");
+    writer.Write("Hello, world!");
+    writer.Flush();
+}
---
+++ Homework4/SimpleFTP/SimpleFTPServer/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Net.Sockets;
+using System.Net;
+
+const int port = 8888;
+var listener = new TcpListener(IPAddress.Any, port);
+listener.Start();
+Console.WriteLine($"Listening on port {port}...");
+using (var socket = listener.AcceptSocket())
+{
+    var stream = new NetworkStream(socket);
+    var streamReader = new StreamReader(stream);
+    var data = streamReader.ReadToEnd();
+    Console.WriteLine($"Received: {data}");
+}
+
+listener.Stop();

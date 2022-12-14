152c40059ec0b8528a887ef20ee5c0e3c2fed5c4
YuriUfimtsev <ufimtsev3007@gmail.com>
2022-12-14 12:34:15 +0300

    initial commit: without tests

---
+++ Exam2/NetworkChat/Client.cs
@@ -0,0 +1,35 @@
+﻿using System.Net.Sockets;
+
+namespace NetworkChat;
+
+public static class Client
+{
+    public static async Task Run(int port, string IPaddress)
+    {
+        using (var tcpClient = new TcpClient(IPaddress, port))
+        {
+            var stream = tcpClient.GetStream();
+            var writer = new StreamWriter(stream);
+            var reader = new StreamReader(stream);
+            while (true)
+            {
+                var message = Console.ReadLine();
+                await writer.WriteLineAsync(message);
+                await writer.FlushAsync();
+
+                if (message == "exit")
+                {
+                    break;
+                }
+
+                Console.WriteLine($"Sent: {message}");
+                var data = await reader.ReadLineAsync();
+                Console.WriteLine($"Received: {data}");
+                if (data == "exit")
+                {
+                    break;
+                }
+            }
+        }
+    }
+}
---
+++ Exam2/NetworkChat/Server.cs
@@ -0,0 +1,40 @@
+﻿using System.Net.Sockets;
+using System.Net;
+
+namespace NetworkChat;
+
+public static class Server
+{
+    public static async Task Run(int port)
+    {
+        var listener = new TcpListener(IPAddress.Any, port);
+        listener.Start();
+        Console.WriteLine("Ready to communicate");
+        var socket = await listener.AcceptSocketAsync();
+        Console.WriteLine("The connection is established");
+        var stream = new NetworkStream(socket);
+        var reader = new StreamReader(stream);
+        var writer = new StreamWriter(stream);
+        while (true)
+        {
+            var data = await reader.ReadLineAsync();
+            if (data == "exit")
+            {
+                break;
+            }
+
+            Console.WriteLine($"received: {data}");
+
+            var response = Console.ReadLine();
+            await writer.WriteLineAsync(response);
+            await writer.FlushAsync();
+            Console.WriteLine($"Sent: {response}");
+            if (response == "exit")
+            {
+                break;
+            }
+        }
+
+        socket.Close();
+    }
+}

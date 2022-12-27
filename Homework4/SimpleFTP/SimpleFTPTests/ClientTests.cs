c20b0a390c8fdcf1a39afe264be7ffe9ced97fbe
YuriUfimtsev <ufimtsev3007@gmail.com>
2022-12-27 12:49:45 +0300

    fix: add copy to buffer

--- Homework4/SimpleFTP/SimpleFTPTests/ClientTests.cs
+++ Homework4/SimpleFTP/SimpleFTPTests/ClientTests.cs
@@ -45,20 +45,6 @@ public class ClientTests
     }
 
     [Test]
-    public async Task StandartGetRequestTest()
-    {
-        var client = new Client(8887);
-        Task.Run(async () => await ServerStartMoq(8887, cancellationTokenSource.Token));
-        using var resultStream = new MemoryStream();
-        await client.Get(pathForTestGetRequest, resultStream);
-        cancellationTokenSource.Cancel();
-        var expectedResult = System.Text.Encoding.Default.GetString(textFileBytes);
-        var streamReader = new StreamReader(resultStream);
-        var result = await streamReader.ReadToEndAsync();
-        Assert.That(result == expectedResult);
-    }
-
-    [Test]
     public async Task GetRequestTestWithEndOfLineCharacterInTheMiddle()
     {
         var client = new Client(8889);
